2dc27147a838194f48c286276c88422b549c1f0d
Nan0MK <nan0mk@outlook.com>
2023-05-18 06:26:46 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# zig-pwsh
+ A basic Zig project with a powershell script merely to run the build command
---
+++ src/main.zig
@@ -0,0 +1,5 @@
+const std = @import("std");
+
+pub fn main() !void {
+    std.debug.print("Nan0MK's {s}, and {s} basic project!\n", .{ "zig", "powershell" });
+}
